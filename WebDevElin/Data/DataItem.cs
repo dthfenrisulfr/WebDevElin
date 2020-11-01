a8fe473004dbbc9e81b999a71880df09cbe4e699
Станислав <baskervilo88@gmail.com>
2020-11-01 18:17:30 +0600

    Функционал виджетов

---
+++ WebDevElin/Data/DataItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDevElin.Data
+{
+  class DataItem
+  {
+    public DateTime Date { get; set; }
+    public int Value { get; set; }
+  }
+}
---
+++ WebDevElin/Data/DiagnosticInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WebDevElin.Data
+{
+  public static class DiagnosticInfo
+  {
+    private static PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+    private static PerformanceCounter ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+
+    public static ushort GetCPULoad()
+    {
+      return (ushort)cpuCounter.NextValue();
+    }
+
+    public static int GetRAMLoad()
+    {
+      return (int)ramCounter.NextValue();
+    }
+
+    public static  List<string> GetLogs()
+    {
+      var list = new List<string>();
+      var eventlog = EventLog.GetEventLogs("DESKTOP-O0O4A8D");
+
+      foreach (var log in eventlog)
+      {
+        foreach (var entry in log.Entries.OfType<EventLogEntry>().TakeLast(1))
+        {
+          list.Add(entry.Message);
+        }
+      }
+
+      return list;
+    }
+  }
+}
